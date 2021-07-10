f197bb874e66ccf6d727cb5e82927a734d8ad364
Sivachandran <svachantowin@gmail.com>
2021-07-10 12:41:49 +0530

    [ROOM-SERVICE] - Uploading the sample application of spring rest server

---
+++ spring-cloud/room-reservation-service/src/test/java/com/techstart/roomreservationservice/RoomReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstart.roomreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
