9ee3990dd426f64e1dcb3b049489b312e41d46e3
Sivachandran <svachantowin@gmail.com>
2021-07-10 12:33:06 +0530

    [REDIS-CACHE] Updating the sample application of redis cache

---
+++ cache/rediscache/src/test/java/com/techstart/rediscache/RediscacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstart.rediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RediscacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
